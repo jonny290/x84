c5886933f74467bb87e33976efa57126b055741c
Matt Zorzin <matt.zorzin@sendgrid.com>
2015-03-09 19:42:51 -0600

    bug fixing

--- x84/default/main.py
+++ x84/default/main.py
@@ -231,6 +231,7 @@ def fillwindow(window, fillchar='#',bordered=False):
 def main():
     """ Main menu entry point. """
     import os, glob, random
+    from x84.bbs import showart
     session, term = getsession(), getterminal()
 
     text, width, height, dirty = u'', -1, -1, 2
