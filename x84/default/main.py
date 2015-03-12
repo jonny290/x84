dbcd3f458bf3f65e853da4490ddd14b0d23b3e35
Matt Zorzin <matt.zorzin@sendgrid.com>
2015-03-11 18:23:28 -0600

    bug fixing

--- x84/default/main.py
+++ x84/default/main.py
@@ -237,13 +237,14 @@ def renderscreen(menudraw=True, artdraw=True, bgdraw=True, tall=False, wide=Fals
     colors['border'] = term.green
     #lets start with the bg frame
     headers = glob.glob(os.path.join(here,"art","top","*.*"))
+    artfile = art_file
     background = AnsiWindow(term.height - 1, term.width, 0, 0)
     background.init_theme(colors, None, 'double')
     echo(term.clear())
     if time.time() - walltime > 60:
-        art_file = headers[random.randrange(0,len(headers))]
+        artfile = headers[random.randrange(0,len(headers))]
     ypos = 1
-    for line in showart(art_file, encoding=art_encoding,):
+    for line in showart(artfile, encoding=art_encoding,):
 	if ypos >= term.height - 3:
 	    break
 	echo(background.pos(ypos, 2)+line)
