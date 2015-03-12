9d6061a774275f7fd36b2cf2b860b60cffffcd0f
Matt Zorzin <matt.zorzin@sendgrid.com>
2015-03-11 18:46:51 -0600

    bug fixing

--- x84/default/main.py
+++ x84/default/main.py
@@ -461,7 +461,7 @@ def main():
                 else:
                     echo(editor.process_keystroke(inp))
                 inp = term.inkey(0)
-        if time.time() - starttime > 300:
+        if time.time() - starttime > 10:
             dirty = True
             starttime = time.time()
             continue
